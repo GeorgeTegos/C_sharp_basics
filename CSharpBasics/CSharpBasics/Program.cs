e30c8a061234acfcc579fab1478aad08f9268a36
George Tegos <george.tegos.22@gmail.com>
2024-01-01 20:02:42 +0000

    Boilerplate code to start

--- CSharpBasics/CSharpBasics/Program.cs
+++ CSharpBasics/CSharpBasics/Program.cs
@@ -1,2 +1,13 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+namespace CSharpBasics
+{
+    public class Program
+    {
+        // Basic code to run Main
+        static void Main(string[] args)
+        {
+            Console.WriteLine("This is a console Text test");
+        }
+    }
+}
